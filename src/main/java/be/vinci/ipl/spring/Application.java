fbefe7ac442ac90409d2d07117c34cb632abfdd8
kyle <trong.nguyen@student.vinci.be>
2024-09-22 18:51:03 +0200

    Init

---
+++ src/main/java/be/vinci/ipl/spring/Application.java
@@ -0,0 +1,13 @@
+package be.vinci.ipl.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
